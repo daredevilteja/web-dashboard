a40989aa4c0f68cd0fbfb3f19e1e23daf9d5d317
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-07-31 00:16:38 +0530

    add user functionality completed

--- src/AddModal.js
+++ src/AddModal.js
@@ -1,68 +1,89 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import {Modal, Button} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import {useState} from "react";
 
 // import './styles.css'
 
 function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Add Lead</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form className={"add_lead_form"}>
-          <label htmlFor="first_name">First name:</label>
-          <br />
-          <input type="text" id="first_name" name="first_name" />
-          <br />
-          <label htmlFor="last_name">Last name:</label>
-          <br />
-          <input type="text" id="last_name" name="last_name" />
-          <br />
-          <label htmlFor="email">Email Address:</label>
-          <br />
-          <input type="email" id="email" name="email" />
-          <br />
-          <label htmlFor="mobile">Mobile:</label>
-          <br />
-          <input type="number" id="mobile" name="mobile" />
-          <br />
-          <label htmlFor="location_type">Location Type:</label>
-          <br />
-          <select id="location_type" name="location_type">
-            <option value="city">City</option>
-            <option value="country">Country</option>
-            <option value="state">State</option>
-          </select>
-        </form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-        <Button>Save</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const newUser = {
+            firstName: event.target[0].value,
+            lastName: event.target[1].value,
+            email: event.target[2].value,
+            mobile: event.target[3].value,
+            locationType: event.target[4].value,
+            locationString: event.target[5].value,
+        };
+
+        props.createNewUser(newUser);
+        props.onHide();
+    };
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">Add Lead</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form className={"add_lead_form"} onSubmit={(event) => submitHandler(event)}>
+                    <label htmlFor="first_name">First name:</label>
+                    <br/>
+                    <input type="text" id="first_name" name="first_name"/>
+                    <br/>
+                    <label htmlFor="last_name">Last name:</label>
+                    <br/>
+                    <input type="text" id="last_name" name="last_name"/>
+                    <br/>
+                    <label htmlFor="email">Email Address:</label>
+                    <br/>
+                    <input type="email" id="email" name="email"/>
+                    <br/>
+                    <label htmlFor="mobile">Mobile:</label>
+                    <br/>
+                    <input type="number" id="mobile" name="mobile"/>
+                    <br/>
+                    <label htmlFor="location_type">Location Type:</label>
+                    <br/>
+                    <select id="location_type" name="location_type">
+                        <option value="city">City</option>
+                        <option value="country">Country</option>
+                        <option value="state">State</option>
+                    </select>
+                    <label htmlFor="location_string">Location String:</label>
+                    <br/>
+                    <input type="text" id="location_string" name="location_string"/>
+                    <br/>
+                    <Button onClick={props.onHide}>Close</Button>
+                    <button type="submit" className={"add_lead_btn"}>Save</button>
+                </form>
+            </Modal.Body>
+            {/*<Modal.Footer>*/}
+            {/*  <Button onClick={props.onHide}>Close</Button>*/}
+            {/*  <Button>Save</Button>*/}
+            {/*</Modal.Footer>*/}
+        </Modal>
+    );
 }
 
 export default function AddModal(props) {
-  const [modalShow, setModalShow] = useState(props.showAdd);
-  if (modalShow === false) {
-    props.closeModal(false);
-  }
+    const [modalShow, setModalShow] = useState(props.showAdd);
+    if (modalShow === false) {
+        props.closeModal(false);
+    }
 
-  return (
-    <>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-    </>
-  );
+    return (
+        <>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                createNewUser={props.createNewUser}
+            />
+        </>
+    );
 }
--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import AddModal from "./AddModal";
 import "./App.css";
 
 function App() {
-  const [tableRowList, updateTableRow] = useState([]);
-  const [addView, setAddView] = useState(false);
+    const [tableRowList, updateTableRow] = useState([]);
+    const [addView, setAddView] = useState(false);
 
-  function getModal(val) {
-    setAddView(val);
-  }
+    function getModal(val) {
+        setAddView(val);
+    }
 
-  return !addView ? (
-    <div className="App">
-      <button onClick={() => setAddView(true)}>Add</button>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Mobile Number</th>
-          <th>Location Type</th>
-          <th>Location String</th>
+    const createUser = (val) => {
+        let newUser = val;
+        tableRowList.push(newUser);
+        updateTableRow(tableRowList);
+    }
 
-          <th>Action</th>
-        </tr>
-        {tableRowList.map((val, idx) => (
-          <tr>
-            <td>val.name</td>
-            <td>val.email</td>
-            <td>val.mobNum</td>
-            <td>val.locType</td>
-            <td>val.locStr</td>
-            <td>
-              <button>Mark Update</button>
-              <button>Delete</button>
-            </td>
-          </tr>
-        ))}
-      </table>
-    </div>
-  ) : (
-    <AddModal showAdd={addView} closeModal={getModal} />
-  );
+    return !addView ? (
+        <div className="App">
+            <button onClick={() => setAddView(true)}>Add</button>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile Number</th>
+                    <th>Location Type</th>
+                    <th>Location String</th>
+
+                    <th>Action</th>
+                </tr>
+                {tableRowList.map((val, idx) => (
+                    <tr>
+                        <td>{val.firstName + " " + val.lastName}</td>
+                        <td>{val.email}</td>
+                        <td>{val.mobile}</td>
+                        <td>{val.locationType}</td>
+                        <td>{val.locationString}</td>
+                        <td>
+                            <button>Mark Update</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    ) : (
+        <AddModal showAdd={addView} closeModal={getModal} createNewUser={createUser}/>
+    );
 }
 
 export default App;
